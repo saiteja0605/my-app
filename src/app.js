b65a61959aefa3a0c2b48861eb58580bca52a41b
saiteja0605 <saitejasaiii06@gmail.com>
2025-04-20 08:58:45 +0530

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 8080; 
+
+app.get('/', (req, res) => {
+  res.send('Hello! This is finocplus CI-CD Pipeline');
+});
+
+const server = app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://65.2.177.67:${PORT}`);
+});
+
+module.exports = server;
